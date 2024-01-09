c162d5e6458915a33e955897de1c17631dc6a55c
Gurpreet Singh Deol <110342143+GurpreetSDeol@users.noreply.github.com>
2024-01-09 22:18:40 +0000

    Add files via upload

--- Plotter.py
+++ Plotter.py
@@ -14,8 +14,8 @@ class Plotter:
 
   
   
-    def multiple_histogram_plot(self, num_cols):
-        f = pd.melt(self.df, value_vars=num_cols)
+    def multiple_histogram_plot(self, num_columns):
+        f = pd.melt(self.df, value_vars=num_columns)
         g = sns.FacetGrid(f, col="variable", col_wrap=4,
                           sharex=False, sharey=False)
         g = g.map(sns.histplot, "value", kde=True)
@@ -28,30 +28,26 @@ class Plotter:
         pyplot.show()
 
    
-    def multiple_qq_plot(self, cols):
-        remainder = 1 if len(cols) % 4 != 0 else 0
-        rows = int(len(cols) / 4 + remainder)
+    def multiple_qq_plot(self, columns):
+        remainder = 1 if len(columns) % 4 != 0 else 0
+        rows = int(len(columns) / 4 + remainder)
 
         fig, axes = pyplot.subplots(
             ncols=4, nrows=rows, sharex=False, figsize=(12, 6))
-        for col, ax in zip(cols, np.ravel(axes)):
+        for col, ax in zip(columns, np.ravel(axes)):
             sm.qqplot(self.df[col], line='s', ax=ax, fit=True)
             ax.set_title(f'{col} QQ Plot')
         pyplot.tight_layout()
     
     
-    def agostino_k2_test(self, column):
-        stat, p = normaltest(self.df[column], nan_policy='omit')
-        print('Statistics=%.3f, p=%.3f' % (stat, p))
-
 
     def missing_nulls_plot(self):
         msno.matrix(self.df)
         pyplot.show()
 
   
-    def correlated_variables(self, cols):
-        corr = self.df[cols].corr()
+    def correlated_variables(self, columns):
+        corr = self.df[columns].corr()
         mask = np.zeros_like(corr)
         mask[np.triu_indices_from(mask)] = True
         pyplot.figure(figsize=(10, 8))
